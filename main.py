71a4c006efc9a12ec2cea8cc9e802c8dfcba496c
Ghanie <abdallahabdulghany@gmail.com>
2022-08-26 14:59:11 +0200

    Adding function to generate output video name and handling errors in video url

--- main.py
+++ main.py
@@ -14,9 +14,39 @@ class VideoConnectionError(Exception):
     def __init__(self):
         print("Connection Error......")
 
+
+def generateRandomNumber(min_value: int, max_value: int):
+    """
+    :param min_value:
+    :param max_value:
+    :return: random Number to be concatenated with the file name
+    """
+    return random.randint(min_value, max_value)
+
+
+def generteRandomFileName(extension: str = '.mp4', delimiter: str = '/', url: str = " ") -> str:
+    """
+
+    :param url:
+    :param extension:
+    :param delimiter:
+    :param url:
+    :return:
+    """
+    folder_path: str = 'Downloads/'
+    video_name = re.sub('[^A-Za-z0-9]+', '', url.split(delimiter)[-1])
+    output_file = "{0}{1}{2}".format(folder_path, video_name, extension)
+    while pathlib.Path(output_file).exists():
+        video_name = (str(generateRandomNumber(0, 999))).join(video_name)
+        output_file = "{0}{1}{2}".format(folder_path, video_name, extension)
+    print(output_file)
+    return output_file
+
+
 if __name__ == '__main__':
     print('>>> Running in DEV MODE...')
     user_input = input('Enter a URL:')
+    file_name = generteRandomFileName(url=user_input)
     try:
         urllib.request.urlopen(user_input)
     except Exception:
@@ -24,7 +54,7 @@ if __name__ == '__main__':
 
     # Download
     try:
-        ydl_opts = {'outtmpl': 'file_name.mp4'}
+        ydl_opts = {'outtmpl': file_name}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([user_input])
     except yt_dlp.utils.DownloadError:
